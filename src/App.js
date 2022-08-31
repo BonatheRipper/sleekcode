87fdb18e8e529b3293a2ab20708faaee8b2e6282
bonatheripper <izeoguandrew@gmail.com>
2022-08-31 16:58:01 +0100

    added tailwing css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Home } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 const App = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import Wave from "react-wavify";
+import Navbar from "../components/Navbar";
 
-const Homes = () => {
-  const style = {
-    transform: "rotate(180deg)",
-    margin: 0,
-    paddin: 0,
-  };
+const Home = () => {
   return (
-    <div style={style}>
-      <Wave
-        fill="#1277b0"
-        paused={false}
-        options={{ height: 80, amplitude: 40, speed: 0.08, points: 4 }}
-      />
+    <div className="mainNight main">
+      <div className="slantedA slantedDay">
+        {" "}
+        <Navbar />
+      </div>
     </div>
   );
 };
 
-export default Homes;
+export default Home;
