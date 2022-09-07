55d7a4b98e4a37e8ee89f8105bde7d532d43c182
bonatheripper <izeoguandrew@gmail.com>
2022-09-07 23:35:17 +0100

    working on probels page

--- src/components/SideBarQuestion.jsx
+++ src/components/SideBarQuestion.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { useGlobalContext } from "../utils/GlobalContext";
 import { demoQuestions } from "../seed/demoData";
-import { useState } from "react";
-import { useEffect } from "react";
 import { MdArrowForwardIos, MdArrowBackIosNew } from "react-icons/md";
 
 const SideBarQuestion = ({ problemsBtn }) => {
   const { day } = useGlobalContext();
-  const [holdDiffulty, setholdDiffulty] = useState(null);
-  useEffect(() => {
-    setholdDiffulty(Difficulty);
-  }, []);
+
   const Difficulty = () => {
     const difficultyArray = [Easy(), Medium(), Hard()];
 
@@ -53,7 +48,7 @@ const SideBarQuestion = ({ problemsBtn }) => {
         </div>
       );
     }
-    return difficultyArray;
+    return difficultyArray[Math.floor(Math.random() * difficultyArray.length)];
   };
   //    bg - cyan - 500;
   return (
@@ -64,44 +59,45 @@ const SideBarQuestion = ({ problemsBtn }) => {
     >
       <div className="questionsContainer relative w-full h-auto ">
         <div className="Questions  flex flex-col h-full">
-          {demoQuestions.map((question, index) => (
+          {demoQuestions.slice(0, 35).map((question, index) => (
             <div
               key={question}
               className={`border ${
                 !day ? " border-gray-700 " : "border-gray-200"
               } ${
-                index === 36 ? "pb-28 " : " "
+                index === 35 ? "pb-28 " : " "
               }  px-2 md:px-4 w-full flex py-2 flex-row justify-between items-center text-xs md:text-sm`}
             >
               <div className="QuestionInner flex flex-row ">
                 {/* <span className="pr-2 md:pr-3 ">{index + 1}</span> */}
                 <span>{question}</span>
               </div>
-              {holdDiffulty &&
-                holdDiffulty[Math.floor(Math.random() * holdDiffulty.length)]}
+              {Difficulty()}
             </div>
           ))}
-          <div
-            className={` ${
-              !day
-                ? " border-gray-700 mainNight"
-                : "border-gray-200 bg-gray-100"
-            }  w-full md:w-6/12 text-xs md:text-base  border fixed bottom-0 py-3 px-2 flex flex-row justify-between items-center`}
-          >
-            <div className="flex font-bold flex-row justify-between items-center">
-              <span className="px-2">
-                {" "}
-                <MdArrowBackIosNew />
-              </span>
-              <span>Previous</span>
+          {problemsBtn.isActive && (
+            <div
+              className={` ${
+                !day
+                  ? " border-gray-700 mainNight"
+                  : "border-gray-200 bg-gray-100"
+              }  w-full md:w-6/12 text-xs md:text-base  border fixed bottom-0 py-3 px-2 flex flex-row justify-between items-center`}
+            >
+              <div className="flex font-bold flex-row justify-between items-center">
+                <span className="px-2">
+                  {" "}
+                  <MdArrowBackIosNew />
+                </span>
+                <span>Previous</span>
+              </div>
+              <div className="flex flex-row justify-between items-center">
+                <span>Next</span>
+                <span className="px-2 font-bold">
+                  <MdArrowForwardIos />
+                </span>
+              </div>
             </div>
-            <div className="flex flex-row justify-between items-center">
-              <span>Next</span>
-              <span className="px-2 font-bold">
-                <MdArrowForwardIos />
-              </span>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
